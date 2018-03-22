950261ce66fb5f00e7dc12ab3dae88866f6e4ee3
Cayo Aguiar do Nascimento <cayocan@hotmail.com>
2018-03-22 02:33:34 -0300

    Canvas da tela de seleção de áudio pronta

---
+++ Assets/Scripts/ButtonGridYpos.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonGridYpos : MonoBehaviour {
+    public RectTransform rect;
+
+	void OnEnable ()
+    {
+        StartCoroutine(FixPosY());   
+	}
+
+    IEnumerator FixPosY()
+    {
+        yield return new WaitForSeconds(0.1f);
+        rect.anchoredPosition = new Vector2(rect.anchoredPosition.x, (rect.sizeDelta.y / 2) * -1);
+    }
+}
